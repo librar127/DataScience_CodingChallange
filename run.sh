1ac65f4b0a2d0af838a41b58ccfe77615efec10b
librar127 <rajneesh.k1990@gmail.com>
2015-11-04 21:30:29 +0530

    updating run.sh post testing

---
+++ run.sh
@@ -0,0 +1,6 @@
+
+echo -e "\n--------Executing tweets_cleaned.java --------"
+java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.tweets_cleaned
+
+echo -e "\n--------Executing average_degree.java --------"
+java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.average_degree
