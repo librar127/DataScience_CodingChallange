d7b27e0092cab9d227aeb9ee50c5701146396579
librar127 <rajneesh.k1990@gmail.com>
2015-11-05 01:42:51 +0530

    updated with inputfile as parameter

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 echo -e "\n--------Executing tweets_cleaned.java --------"
-java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.tweets_cleaned
+java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.tweets_cleaned ./tweet_input/tweet.txt
 
 echo -e "\n--------Executing average_degree.java --------"
-java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.average_degree
+java -cp IDS_Coding_Challange.jar InsightDataScience_coding_challenge.average_degree ./tweet_input/tweet.txt
