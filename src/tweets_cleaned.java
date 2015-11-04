ad1eab53ab6cb12bf19ffa69ad378d4f093b1d9d
RajneeshKumar <rajneesh.k1990@gmail.com>
2015-11-05 01:49:57 +0530

    Code for tweet_cleaned Updated

--- src/tweets_cleaned.java
+++ src/tweets_cleaned.java
@@ -14,8 +14,14 @@ import org.json.JSONObject;
 public class tweets_cleaned {
 	
 	CharsetEncoder asciiEncoder = Charset.forName("US-ASCII").newEncoder(); 
+	static int noOfLinewithUnicodeChar = 0;
 
-	// Check if a String is Pure ascii
+	/**
+	 * This method is used to check if a String is Pure ASCII
+	 * 
+	 * @param 	str 	Input string provided to check if it contains unicode characters
+	 * @return  boolean Returns true in case string is pure ASCII else false
+	 */
 	public boolean isPureAscii(String str) {
 		
 		return asciiEncoder.canEncode(str);
@@ -23,50 +29,36 @@ public class tweets_cleaned {
 
 	public static void main(String[] args) {
 		
-		tweets_cleaned tweet = new tweets_cleaned();
+		if(args.length < 1){
+			System.out.println("Usage: provide filename containing tweets");
+			System.exit(0);
+		}
+		
+		tweets_cleaned tc = new tweets_cleaned();
 		
 		try {
 			
-			BufferedReader br = new BufferedReader(new FileReader("./tweet_input/tweets.txt"));
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
 			PrintWriter pw = new PrintWriter(new FileWriter("./tweet_output/ft1.txt"));
 			
 			String jsonLine;
-			String text = null;
-			String timestamp = null;
-			int noOfLinewithUnicodeChar = 0;
+			String[] cleanedTweet;
 					
 			while((jsonLine = br.readLine())!=null){
+
+				cleanedTweet = tc.cleanTweets(jsonLine);
 				
-				JSONObject obj = new JSONObject(jsonLine);
-				
-				// Get the text field from json tweet only if it is present
-				if(obj.has("text")){
-					
-					// Remove all Escape characters from String line
-					text = obj.getString("text").replaceAll("[\\n\\r\\t]", "").replaceAll("(?<=/)[^/]+?(?=/)", "");
-					
-					// Count and remove unicode characters
-					if(!tweet.isPureAscii(text)){
-						
-						noOfLinewithUnicodeChar += 1;
-						text = text.replaceAll("[^\\p{ASCII}]", "");
-					}
-				}
-				
-				// Get the created_at field from json tweet only if it is present
-				if(obj.has("created_at")){
-					timestamp = obj.getString("created_at");//.replaceAll("\\P{Print}", "");//.replaceAll("(?<=/)[^/]+?(?=/)", "");
-				}
-				
-				System.out.println(text+" "+ "(timestamp: "+timestamp+")");
-				// Write the cleaned output to output file ft1.txt
-				pw.println(text+" "+ "(timestamp: "+timestamp+")");
+				if(cleanedTweet[0] != null && cleanedTweet[1] != null)				
+					pw.println(cleanedTweet[0]+" (timestamp: "+cleanedTweet[1]+")");
 
 			}
+			
 			pw.println("\n\n"+ noOfLinewithUnicodeChar + " tweets contained unicode.");
 
-			// Write the unicode characters count output file ft1.txt
-			System.out.println("\n"+ noOfLinewithUnicodeChar + " tweets contained unicode.");
+			/**
+			 * Write the unicode characters count output file ft1.txt
+			 */
+			System.out.println("Output written to tweet_output/ft1.txt"+"\n"+ noOfLinewithUnicodeChar + " tweets contained unicode.");
 			
 			br.close();
 			pw.close();
@@ -77,4 +69,54 @@ public class tweets_cleaned {
 		}
 
 	}
+	
+	/**
+	 * This methods cleans tweets after removing escape characters and unicode characters and then separating text and timestamp.
+	 * 
+	 * @param tweetLine One Single Line of tweets
+	 * @return List of cleaned Text and timestamp after separating them from tweetline
+	 */
+	
+	private String[] cleanTweets(String tweetLine) {
+		
+		tweets_cleaned tc = new tweets_cleaned();
+		
+		JSONObject obj = new JSONObject(tweetLine);
+		String text = null;
+		String timestamp = null;
+		String[] textAndTimestamp = new String[2];
+		
+		/**
+		 * Get the text field from json tweet only if it is present
+		 */
+		if(obj.has("text")){
+			
+			/**
+			 * Remove all Escape characters from String line
+			 */
+			text = obj.getString("text").replaceAll("[\\n\\r\\t]", "").replaceAll("(?<=/)[^/]+?(?=/)", "");
+			
+			/**
+			 * Count and remove unicode characters
+			 */
+			if(!tc.isPureAscii(text)){		
+				
+				noOfLinewithUnicodeChar += 1;
+				text = text.replaceAll("[^\\p{ASCII}]", "");
+			}
+		}
+		
+		/**
+		 * Get the created_at field from json tweet only if it is present
+		 */
+		if(obj.has("created_at")){
+			timestamp = obj.getString("created_at").replaceAll("(?<=/)[^/]+?(?=/)", "");
+		}
+
+		textAndTimestamp[0] = text;
+		textAndTimestamp[1] = timestamp;
+		
+		return textAndTimestamp;
+	}
+
 }
